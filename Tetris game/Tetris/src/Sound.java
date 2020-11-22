85fed014e4f84388e305e2f364ca3e5563a4c04d
Laura Guo <guoitf@gmail.com>
2020-11-22 16:24:37 -0600

    Create Sound.java
    Moved this file into the src folder. Also made some modifications based on the Tetris song choice (lowered the default volume because it is loud) and based on the SoundTests. All tests passed for this class.

---
+++ Tetris game/Tetris/src/Sound.java
@@ -0,0 +1,48 @@
+
+import javafx.scene.media.Media; 
+import javafx.scene.media.MediaPlayer; 
+import java.io.*;
+
+public class Sound {
+    private double volume;
+    private static String music = "tetrisSong.mp3";
+    final double VOLUME_CHANGE_AMOUNT=0.1;
+    Media song; 
+    MediaPlayer player;
+    
+    public Sound() {
+        this(0.25);
+    }
+    public Sound(double volume)
+    {
+        File songFile = new File(music);
+    	this.volume=volume;
+    	song=new Media(songFile.toURI().toString());
+    	player=new MediaPlayer(song);
+    }
+   
+    public void increaseVolume()
+    {
+    	if(volume<1)
+    		this.volume+=VOLUME_CHANGE_AMOUNT;
+    }
+    public void decreaseVolume()
+    {
+    	if(volume>0)
+    		volume-=VOLUME_CHANGE_AMOUNT;
+    }
+    
+    public void playSong() 
+    {
+        player.play();
+    }
+   
+    public double getVolume() {
+        return volume;
+    }
+    
+    public void setVolume(double v) {
+        volume = v;
+    }
+    
+}
