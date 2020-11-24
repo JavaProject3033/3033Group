a9d26901be7c5104348a1dbe5d713a741b74d735
Laura Guo <guoitf@gmail.com>
2020-11-23 23:12:37 -0600

    Update Block.java
    Modifed/troubleshooted the code based on the BlockTest class. All tests passed.

--- Tetris game/Tetris/src/Block.java
+++ Tetris game/Tetris/src/Block.java
@@ -1,31 +1,36 @@
+import java.util.Arrays;
+
+import javafx.scene.paint.*;
 
 class Block {
-	BlockColor color;
-	Shape blockShape;
-	int points[8];
-	int orientation;
+	private BlockColor color;
+	private Shape shape;
+	private int[] points;
+	private int orientation;
 	
 	Block() {
 		color = new BlockColor(0);
 		shape = new Shape('O', 0);
+		points = new int[8];
 		points[0] = 0;
 		points[1] = 0;
 		orientation = 0;
 	}
 	
-	Block(BlockColor inputColor, Shape inputShape, int x, int y, int inputOrientation) {
+	Block(BlockColor inputColor, Shape inputShape, int r, int c, int inputOrientation) {
 		color = inputColor;
-		blockShape = inputShape;
+		shape = inputShape;
 		orientation = inputOrientation;
-		updatePoints(x, y);
+		points = new int[8];
+		updatePoints(r, c);
 	}
 	
-	int[] getPoint() {
+	int[] getPoints() {
 		return points;
 	}
 	
-	void setPoint(int point, int value) {
-		points[point] = value;
+	void setPoints(int [] p) {
+		points = p;
 	}
 	
 	int getOrientation() {
@@ -36,43 +41,73 @@ class Block {
 		orientation = newOrientation;
 	}
 	
-	BlockColor getColor() {
-		return color;
+	Color getColor() {
+		return BlockColor.COLORS[color.getColor()];
 	}
 	
 	void setColor(BlockColor newColor) {
 		color = newColor;
 	}
+	  
+	int getColorNum() {
+        return color.getColor();
+    }
 	
 	boolean[][] getShape() {
-		return blockShape.getShape();
+		return shape.getShape();
 	}
 	
 	void setShape(Shape newShape) {
-		blockShape = newShape;
+		shape = newShape;
 	}
+	
+	Shape getShapeObj() {
+	    return shape;
+	}
+	
 	void rotate() {
-		int height = blockShape.getHeight();
-		int width = blockShape.getWidth();
+	    int[] originalFirstPoint = new int[2];
+	    boolean[][] originalShape = shape.getShape();
+	    boolean found = false;
+	    
+	    for(int r = 0; r < originalShape.length; r ++) {
+	        for(int c = 0; c < originalShape[0].length; c ++) {
+	            if(originalShape[r][c]) {
+	                originalFirstPoint[0] = r;
+	                originalFirstPoint[1] = c;
+	                found = true;
+	                break;
+	            } // end if
+	        } // end for
+	        
+	        if(found) {
+	            break;
+	        } // end if
+	    } // end for
+	    
+	    int originRow = points[0] - originalFirstPoint[0]; // first row of the shape array (NOT the first block in the shape)
+	    int originCol = points[1] - originalFirstPoint[1]; // first column of the shape array
+	    
 		int i = 0;
 		int j = 0;
 		boolean pointFound = false;
 		int tempPointX = 0;
 		int tempPointY = 0;
-		blockShape.setShape(orientation);
-		boolean[height][width] tempShape = blockShape.getShape();
-		for(i = 0, i < height, i++) {
-			for(j = 0, j < width, j++) {
+		shape.setShape(orientation);
+		boolean[][] tempShape = shape.getShape();
+		for(i = 0; i < tempShape.length; i++) {
+			for(j = 0; j < tempShape[0].length; j++) {
 				if(!pointFound) {
-					if (tempShape[i][j] == 1) {
+					if (tempShape[i][j] == true) {
 						pointFound = true;
-						tempPointX = j;
-						tempPointY = i;
+						tempPointX = i;
+						tempPointY = j;
 					}
 				}
 			}
 		}
-		updatePoints(tempPointX, tempPointY);
+	
+		updatePoints(tempPointX + originRow, tempPointY + originCol);
 	}
 	void rotateRight() {
 		orientation++;
@@ -91,44 +126,47 @@ class Block {
 	}
 	
 	void moveLeft() {
-		if(points[0] > 0 && points[2] > 0 && points[4] > 0 && points[6] > 0) {
-			points[0]--;
-			points[2]--;
-			points[4]--;
-			points[6]--;
+		if(points[1] > 0 && points[3] > 0 && points[5] > 0 && points[7] > 0) {
+			points[1]--;
+			points[3]--;
+			points[5]--;
+			points[7]--;
 		}
 	}
 	
 	void moveRight() {
-		if(points[0] < 9 && points[2] < 9 && points[4] < 9 && points[6] < 9) {
-			points[0]++;
-			points[2]++;
-			points[4]++;
-			points[6]++;
+		if(points[1] < 9 && points[3] < 9 && points[5] < 9 && points[7] < 9) {
+			points[1]++;
+			points[3]++;
+			points[5]++;
+			points[7]++;
 		}
 	}
 	
 	boolean isSameColor(Block otherBlock) {
-		return color.isEqual(otherBlock.getColor());
+		return this.getColorNum() == otherBlock.getColorNum();
 	}
 	
-	private void updatePoints(int newX, int newY) {
-		int height = blockShape.getHeight();
-		int width = blockShape.getWidth();
-		boolean[height][width] currentShape = blockShape.getShape();
+	public void updatePoints(int newR, int newC) {
+		boolean[][] currentShape = shape.getShape();
 		int currentPoint = 0;
-		for(int i = 0; i < height; i++) {
-			for(int j = 0; j < width; j++) {
+		for(int i = 0; i < currentShape.length; i++) {
+			for(int j = 0; j < currentShape[0].length; j++) {
 				if(currentShape[i][j]) {
-					points[currentPoint] = j;
-					points[currentPoint+1] = i;
+					points[currentPoint] = i;
+					points[currentPoint+1] = j;
 					currentPoint = currentPoint + 2;
+					
 				}
 			}
 		}
-		for(int k = 7; k > 0; k = k - 2) {
-			points[k] = points[k] + newY - points[1];
-			points[k-1] = points[k-1] + newX - points[0];
+
+		for(int k = 2; k < 8; k = k + 2) {
+		    points[k] = points[k] + newR - points[0];
+		    points[k+1] = points[k+1] + newC - points[1];
 		}
+		
+	    points[0] = newR;
+	    points[1] = newC;
 	}
 }
