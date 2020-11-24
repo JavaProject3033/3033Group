f71ab3e20b72af6efde3c6b1a731376fc1cab642
Laura Guo <guoitf@gmail.com>
2020-11-23 20:18:24 -0600

    Update BlockColor.java
    Debugged, ran tests. All tests passed.

--- Tetris game/Tetris/src/BlockColor.java
+++ Tetris game/Tetris/src/BlockColor.java
@@ -1,41 +1,36 @@
 import javafx.scene.paint.Color;
 
 public class BlockColor {
-	public static final NUM_COLORS = 7;
-	public static final Color COLORS[NUM_COLORS];
+	public static final int NUM_COLORS = 7;
+	public static final Color COLORS[] = {
+	        Color.rgb(250, 50, 10), // red
+	        Color.rgb(10, 250, 50), // green
+	        Color.rgb(10, 50, 250), // blue
+	        Color.rgb(250, 160, 10), // orange
+	        Color.rgb(190, 20, 220), // purple
+	        Color.rgb(20, 230, 230), // teal
+	        Color.rgb(250, 250, 0) // yellow
+	};
+	
 	private int color;
 	
 	BlockColor() {
 		color = 0;
-		COLORS[0] = new Color(250, 50, 10); // red
-		COLORS[1] = new Color(10, 250, 50); // green
-		COLORS[2] = new Color(10, 50, 250); // blue
-		COLORS[3] = new Color(250, 160, 10); // orange
-		COLORS[4] = new Color(190, 20, 220); // purple
-		COLORS[5] = new Color(20, 230, 230); // teal
-		COLORS[6] = new Color(250, 250, 0); // yellow
 	}
 	
 	BlockColor(int chosenColor) {
 		color = chosenColor;
-		COLORS[0] = new Color(250, 50, 10); // red
-		COLORS[1] = new Color(10, 250, 50); // green
-		COLORS[2] = new Color(10, 50, 250); // blue
-		COLORS[3] = new Color(250, 160, 10); // orange
-		COLORS[4] = new Color(190, 20, 220); // purple
-		COLORS[5] = new Color(20, 230, 230); // teal
-		COLORS[6] = new Color(250, 250, 0); // yellow
 	}
 	
 	boolean isEqual(BlockColor otherColor) {
 		return otherColor.getColor() == color;
 	}
 	
-	void setColorNum(int newColor) {
+	void setColor(int newColor) {
 		color = newColor;
 	}
 	
-	int getColorNum() {
+	int getColor() {
 		return color;
 	}
 }
