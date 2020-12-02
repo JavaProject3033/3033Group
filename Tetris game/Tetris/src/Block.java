a27d09408fb76a54860878b0c278f419691ca8a6
GeorgeMaddux <gdm2z@mtmail.mtsu.edu>
2020-12-02 15:27:55 -0600

    Update Block.java
    documentation completed

--- Tetris game/Tetris/src/Block.java
+++ Tetris game/Tetris/src/Block.java
@@ -1,14 +1,20 @@
+/**
+* Author George Maddux, Laura Guo
+* Due Date 12/2/2020
+* Class CSCI-3033-001
+* 
+*/
 import java.util.Arrays;
 
 import javafx.scene.paint.*;
 
 class Block {
-	private BlockColor color;
-	private Shape shape;
-	private int[] points;
-	private int orientation;
+	private BlockColor color; 	//contains an array of rgb color values with getters and setters
+	private Shape shape; 		//contains arrays of hard-coded rotations
+	private int[] points; 		//the position of the block on the board
+	private int orientation; 	//describes how the block was rotated, for use with Shape
 	
-	Block() {
+	Block() {					//initializes a default block, chosen arbitrarily
 		color = new BlockColor(0);
 		shape = new Shape('O', 0);
 		points = new int[8];
@@ -18,6 +24,7 @@ class Block {
 	}
 	
 	Block(BlockColor inputColor, Shape inputShape, int r, int c, int inputOrientation) {
+		//initializes a block based on full input of its criteria
 		color = inputColor;
 		shape = inputShape;
 		orientation = inputOrientation;
@@ -65,7 +72,7 @@ class Block {
 	    return shape;
 	}
 	
-	void rotate() {	    
+	void rotate() {	    //driver method for rotating a block
 	    int[] originalFirstPoint = getFirstPoint();
 	    int originRow = points[0] - originalFirstPoint[0]; // first row of the shape array (NOT the first square in the shape)
 	    int originCol = points[1] - originalFirstPoint[1]; // first column of the shape array
@@ -73,9 +80,9 @@ class Block {
 		shape.setShape(orientation);
 		
 		int[] newFirstPoint = getFirstPoint();
-		updatePoints(newFirstPoint[0] + originRow, newFirstPoint[1] + originCol);
+		updatePoints(newFirstPoint[0] + originRow, newFirstPoint[1] + originCol); //updates the position of a block using calculated points offset by the original values
 	}
-	void rotateRight() {
+	void rotateRight() { //increments the orientation variable modulo 4 then calls rotate
 		orientation++;
 		if(orientation == 4) {
 			orientation = 0;
@@ -83,7 +90,7 @@ class Block {
 		rotate();
 	}
 	
-	void rotateLeft() {
+	void rotateLeft() { //decrements the orientation variable modulo 4 then calls rotate
 		orientation--;
 		if(orientation == -1) {
 			orientation = 3;
@@ -109,12 +116,12 @@ class Block {
 		return this.getColorNum() == otherBlock.getColorNum();
 	}
 	
-	public void updatePoints(int newR, int newC) {
+	public void updatePoints(int newR, int newC) { //takes the new position of the corner of a block's shape array and uses that to calculate the position of each square
 		boolean[][] currentShape = shape.getShape();
 		int currentPoint = 0;
 		for(int i = 0; i < currentShape.length; i++) {
 			for(int j = 0; j < currentShape[0].length; j++) {
-				if(currentShape[i][j]) {
+				if(currentShape[i][j]) { //checks the boolean shape array for if there's a square at that coordinate
 					points[currentPoint] = i;
 					points[currentPoint+1] = j;
 					currentPoint = currentPoint + 2;
@@ -124,7 +131,7 @@ class Block {
 		}
 
 		for(int k = 2; k < 8; k = k + 2) {
-		    points[k] = points[k] + newR - points[0];
+		    points[k] = points[k] + newR - points[0]; //calculates the new location using the old one and an offset
 		    points[k+1] = points[k+1] + newC - points[1];
 		}
 		
@@ -132,7 +139,7 @@ class Block {
 	    points[1] = newC;
 	}
 	
-	public boolean equals(Block other) {
+	public boolean equals(Block other) { //uses getters to check if another block is identical in type and position on the board
 	    boolean sameColor = color.getColor() == other.getColorNum();
 	    boolean samePoints = Arrays.equals(points, other.getPoints());
 	    boolean sameShape = shape.getLetter() == other.getShapeObj().getLetter();
@@ -141,7 +148,7 @@ class Block {
 	    return sameColor && samePoints && sameShape && sameOrientation;
 	}
 	
-	public int[] getFirstPoint() {
+	public int[] getFirstPoint() { //returns the coordinate of the top left point of a block
 	    int[] firstPoint = {-1, -1};
         boolean[][] shapeArr = shape.getShape();
         
@@ -158,13 +165,10 @@ class Block {
         return firstPoint;
 	}
 	
-	public int getFirstPointIndex() {
+	public int getFirstPointIndex() { //returns the top left point of a block, in the form of an index value based on its shape array
         int currentPoint = 0;
         boolean[][] shapeArr = shape.getShape();
         boolean[][] normalShapeArr = (new Shape(shape.getLetter(), orientation)).getShape();
-        
-//        System.out.println("current shape arr: \n" + shape);
-//        System.out.println("normal shape arr: \n" + (new Shape(shape.getLetter(), orientation)));
 
         for(int r = 0; r < shapeArr.length; r ++) {
             for(int c = 0; c < shapeArr[0].length; c ++) {
@@ -184,7 +188,7 @@ class Block {
         return -1;
 	}
 	
-	public boolean isValidShapePoint(int r, int c) {
+	public boolean isValidShapePoint(int r, int c) { //returns whether the coordinate is a square of the shape on the board
 	    int[] firstPoint = getFirstPoint();
 	    int firstPointIndex = getFirstPointIndex();
 	    
@@ -197,26 +201,10 @@ class Block {
 	    int shapeRow = r - originPoint[0];
 	    int shapeCol = c - originPoint[1]; 
 
-/*	    System.out.println("\n" + shape.getLetter());
-	    System.out.println(shape);
-	    System.out.println("first pt index: " + firstPointIndex);
-	    System.out.println("point r: " + points[firstPointIndex]);
-        System.out.println("point c: " + points[firstPointIndex + 1]);
-	    System.out.println("first r: " + firstPoint[0]);
-        System.out.println("first c: " + firstPoint[1]);
-        System.out.println("origin r: " + originPoint[0]);
-        System.out.println("origin c: " + originPoint[1]);
-	    System.out.println("given row: " + r);
-	    System.out.println("given col: " + c);
-	    System.out.println("shape row: " + shapeRow);
-	    
-	    System.out.println("shape col: " + shapeCol);*/ 
-
-
 	    return shape.getShape()[shapeRow][shapeCol]; 
 	}
 	
-	public boolean isBlankBlock() {
+	public boolean isBlankBlock() { //returns whether every square of the block has been cleared
 	    boolean isBlankBlock = true;
         boolean[][] shapeArray = shape.getShape();
         
